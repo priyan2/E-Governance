29a0e30df82fd53da43f94539985b7593f604697
Shanmugapriyan <agsshanmu@gmail.com>
2023-05-06 10:15:49 +0530

    Update settings.py
    Updated

--- ComplaintManagementSystem/ComplaintManagementSystem/ComplaintManagementSystem/settings.py
+++ ComplaintManagementSystem/ComplaintManagementSystem/ComplaintManagementSystem/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ubck^*k4u0ed$uxvg%wl3&*ovzo0akj&sqj!*gv56(lr$kvuls'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
